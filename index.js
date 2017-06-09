883ba2349f91b4c5671624de2eab48a7f0b4d7ec
Shannon Thrasher <me@shanimal.com>
2017-06-09 12:13:57 -0700

    eslint complains "no-useless-escape"
    Unnecessary escape character: \#  no-useless-escape. 
    
    Seems like a valid error.

--- index.js
+++ index.js
@@ -6,6 +6,6 @@
  */
 
 module.exports = function isGitUrl(str) {
-  var regex = /(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(.*?)(\.git)(\/?|\#[-\d\w._]+?)$/;
+  var regex = /(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(.*?)(\.git)(\/?|#[-\d\w._]+?)$/;
   return regex.test(str);
 };
